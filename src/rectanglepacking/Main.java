cf61f66ec81d44bf29c2e94e33d32f537766e80c
akbokha <a.k.bokharouss@student.tue.nl>
2017-04-26 12:41:13 +0200

    first commit

---
+++ src/rectanglepacking/Main.java
@@ -0,0 +1,20 @@
+/*
+ * DBL Algorithms - Q4 2016-2017
+ * Rectangle Packing - Group 10
+ */
+package rectanglepacking;
+
+/**
+ *
+ * @author s158881
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
