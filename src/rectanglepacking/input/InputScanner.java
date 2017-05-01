739936aa0f1026725065fa24644d699cba1fe8b2
Addono <a.d.knapen@student.tue.nl>
2017-05-01 15:03:34 +0200

    update: finished output

---
+++ src/rectanglepacking/input/InputScanner.java
@@ -0,0 +1,84 @@
+package rectanglepacking.input;
+
+import rectanglepacking.Area;
+import rectanglepacking.Rectangle;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+/**
+ * @date Apr 26, 2017
+ * @author Adriaan Knapen <a.d.knapen@student.tue.nl>
+ */
+public abstract class InputScanner implements InputInterface {
+
+    private Scanner scanner = null;
+
+    protected void setScanner(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    @Override
+    public Area read() throws IllegalArgumentException, IllegalStateException {
+        if (this.scanner==null) {
+            throw new IllegalStateException("InputScanner.read: scanner should be not null");
+        }
+
+        // Read the container height.
+        scanner.skip("container height: ");
+        String res = scanner.next();
+
+        int height;
+        if (res.equals("free")) {
+            height = 0;
+        } else if (res.equals("fixed")) {
+            height = scanner.nextInt();
+        } else {
+            throw new IllegalArgumentException("invalid container height specification.");
+        }
+
+        // Read flippable allowed.
+        scanner.skip(Pattern.compile("[\\s\\S]*?rotations allowed: "));
+        res = scanner.next();
+
+        boolean flippable;
+        if (res.equals("no")) {
+            flippable = false;
+        } else if (res.equals("yes")) {
+            flippable = true;
+        } else {
+            throw new IllegalArgumentException("invalid flippable specification.");
+        }
+
+        // Read the number of rectangles
+        scanner.skip(Pattern.compile("[\\s\\S]*?number of rectangles: "));
+        int amount;
+        if (scanner.hasNextInt()) {
+            amount = scanner.nextInt(10);
+        } else {
+            throw new IllegalArgumentException("invalid amount of rectangles specified");
+        }
+
+        // Read all rectangles and add them to the area.
+        Area area = new Area(0, height, flippable);
+
+        for (int i = 0; i < amount; i++) {
+            if (! scanner.hasNextInt()) {
+                throw new IllegalArgumentException("not enough rectangles specified");
+            }
+
+            int x = scanner.nextInt();
+
+            if (! scanner.hasNextInt()) {
+                throw new IllegalArgumentException("not enough rectangles specified");
+            }
+
+            int y = scanner.nextInt();
+
+            // Add the new rectangle.
+            area.add(new Rectangle(x, y, -1, -1, flippable));
+        }
+
+        return area;
+    }
+}
--- src/rectanglepacking/output/PlainText.java
+++ src/rectanglepacking/output/PlainText.java
@@ -2,13 +2,18 @@ package rectanglepacking.output;
 
 import rectanglepacking.Area;
 import rectanglepacking.Rectangle;
-
 import java.util.Iterator;
 
 public class PlainText implements OutputInterface {
 
+    private Area area;
+
+    public PlainText(Area area) {
+        this.area = area;
+    }
+
     @Override
-    public void draw(Area area) {
+    public void draw() {
         // Draw the container height line.
         System.out.print("container height: ");
 
@@ -30,10 +35,27 @@ public class PlainText implements OutputInterface {
         // Draw the number of rectangles.
         System.out.println("number of rectangles: " + area.getCount());
 
+        // Iterate through all rectangle to get their dimensions and locations.
         Iterator<Rectangle> i = area.getRectangles();
+        StringBuilder rectangleDimensions = new StringBuilder();
+        StringBuilder rectangleLocations = new StringBuilder();
         while (i.hasNext()) {
             Rectangle rectangle = i.next();
-            System.out.println(rectangle.getX() + " " + rectangle.getY());
+            rectangleDimensions.append(rectangle.getWidth()).append(" ").append(rectangle.getHeight()).append("\n");
+
+            if (area.canFlip()) {
+                if (rectangle.getFlipped()) {
+                    rectangleLocations.append("yes ");
+                } else {
+                    rectangleLocations.append("no ");
+                }
+            }
+            rectangleLocations.append(rectangle.getX()).append(" ").append(rectangle.getY()).append("\n");
         }
+
+        // Print all rectangles
+        System.out.print(rectangleDimensions.toString());
+        System.out.println("placement of rectangles");
+        System.out.print(rectangleLocations.toString());
     }
 }
