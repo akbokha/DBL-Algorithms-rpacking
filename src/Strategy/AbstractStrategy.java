77bf7f64cdc5109116297630c642142d33661834
akbokha <a.k.bokharouss@student.tue.nl>
2017-05-01 14:30:16 +0200

    Strategy Package and AbstractStrategy

---
+++ src/Strategy/AbstractStrategy.java
@@ -0,0 +1,35 @@
+/*
+ * Abstract Strategy
+ * Will be extended by Concrete strategy implementations
+ */
+package Strategy;
+import rectanglepacking.Area;
+
+/**
+ *
+ * @author s158881
+ * 01-05-2017 
+ */
+public abstract class AbstractStrategy {
+    
+    /**
+     * Contains input data
+     */
+    Area area;
+    
+    /**
+     * Constructor
+     * @param area contains input data such as 
+     * Concrete strategies should call super() in constructor
+     */
+    public AbstractStrategy(Area area) {
+        this.area = area;
+    }
+    
+    /**
+     * The actual computation behind the placement of the rectangles
+     * @return an area such that isValid()
+     */
+    public abstract Area ComputeArea();
+    
+}
