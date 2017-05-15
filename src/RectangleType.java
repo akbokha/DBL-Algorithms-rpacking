31e2f3120c937aa6edbd1df00944d95a536e0c16
unknown <s157035@s157035.campus.tue.nl>
2017-05-15 20:17:18 +0200

    Implemented RectangleTypes and combining of similar types

---
+++ src/RectangleType.java
@@ -0,0 +1,61 @@
+/**
+ * Created by s157035 on 12-5-2017.
+ */
+public class RectangleType {
+    private final int width;
+    private final int height;
+    private final boolean canRotate;
+
+    private int instances;
+
+    public RectangleType(int width, int height, boolean canRotate) {
+        this.width = width;
+        this.height = height;
+        //Only allow rotations if the width and height are not equal (rotating does not make any difference then), and if rotations are allowed by input
+        this.canRotate = canRotate && width != height;
+
+        instances = 1;
+    }
+
+    public void increaseInstances() {
+        instances++;
+    }
+
+    public void decreaseInstances() {
+        instances--;
+    }
+
+    public boolean canRotate() {
+        return width == height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getSmallestDimension() {
+        return width > height ? height : width;
+    }
+
+    public int getLargestDimension() {
+        return width > height ? width : height;
+    }
+
+    public int getNumberOfinstances() {
+        return instances;
+    }
+
+    public ADT_Rectangle createInstance() {
+        if(instances <= 0) {
+            throw new IllegalStateException("Tried to create an instance of a rectangle type while there were ");
+        }
+
+        decreaseInstances();
+        ADT_Rectangle rectangle = new ADT_Rectangle(width, height, 0, 0, canRotate());
+        return rectangle;
+    }
+}
---
+++ src/RectangleTypeCombiner.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by s157035 on 12-5-2017.
+ */
+public class RectangleTypeCombiner {
+    ArrayList<RectangleType> types = new ArrayList<>();
+    private boolean rotationAllowed;
+
+    public RectangleType[] combineRectangleTypes(ADT_Area area) {
+        rotationAllowed = area.canFlip();
+
+        //Iterate over the input rectangles
+        for (Iterator<ADT_Rectangle> recs = area.getRectangles(); recs.hasNext();) {
+            ADT_Rectangle currentRec = recs.next();
+
+            //Check if we already have a definition for this rectangle's dimensions
+            RectangleType existingType = containsDimensions(currentRec.getWidth(), currentRec.getHeight());
+
+            if(existingType != null) {
+                //If we have a definition for these dimension already, simply increase it's count
+                existingType.increaseInstances();
+            }
+            else {
+                //If we do not have a definition for this yet, create one and add it to the list
+                RectangleType newType = new RectangleType(currentRec.getWidth(), currentRec.getHeight(), area.canFlip());
+                types.add(newType);
+            }
+        }
+
+        //Create the final result array.
+        RectangleType[] result = new RectangleType[types.size()];
+        for (int i = 0; i < types.size(); i++) {
+            result[i] = types.get(i);
+        }
+        return result;
+    }
+
+    private RectangleType containsDimensions(int width, int height) {
+
+        if(rotationAllowed) {
+            int smallest = width > height ? height : width;
+            int largest = width > height ? width : height;
+            for(int i = 0; i < types.size(); i++) {
+                RectangleType curType = types.get(i);
+
+                if(curType.getSmallestDimension() == smallest && curType.getLargestDimension() == largest) {
+                    return curType;
+                }
+            }
+        }
+        else {
+            for(int i = 0; i < types.size(); i++) {
+                RectangleType curType = types.get(i);
+
+                if(curType.getWidth() == width && curType.getHeight() == height) {
+                    return curType;
+                }
+            }
+        }
+
+        return null;
+    }
+}
