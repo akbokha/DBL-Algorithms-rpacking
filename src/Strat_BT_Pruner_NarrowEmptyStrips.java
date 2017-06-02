7c5edccdec0d9fc0ab3b515503066a6561d452e7
chingchangchong <Dattheman@hotmail.com>
2017-06-02 05:31:31 +0200

    Added a way to create two areas as empty strips, changed some variables to calculate empty strip dimensions
    Quick commit before I go

--- src/Strat_BT_Pruner_NarrowEmptyStrips.java
+++ src/Strat_BT_Pruner_NarrowEmptyStrips.java
@@ -9,47 +9,51 @@ public class Strat_BT_Pruner_NarrowEmptyStrips implements Strat_BT_PrunerInterfa
     private ADT_AreaExtended area;
     private int emptySpaceHeight;
     private int emptySpaceWidth;
+    private ADT_AreaExtended areaSpaceBelow = new ADT_AreaExtended(int calculateEmptySpaceHeight(ADT_AreaExtended area, ADT_Rectangle last),
+    int calculateEmptySpaceWidth(ADT_AreaExtended area, ADT_Rectangle last), boolean flippable); // area of the empty strip
+    private ADT_AreaExtended areaSpaceLeft = new ADT_AreaExtended(int calculateEmptySpaceHeight(ADT_AreaExtended area, ADT_Rectangle last),
+    int calculateEmptySpaceWidth(ADT_AreaExtended area, ADT_Rectangle last), boolean flippable); // area of the empty strip
 
     @Override
-    public boolean reject(ADT_AreaExtended area, ADT_Rectangle last) {
-        this.area = area;
-        emptySpaceHeight = calculateEmptySpaceHeight(area, last); // height of the empty strip on the left of the rectangle
-        emptySpaceWidth = calculateEmptySpaceWidth(area, last); // width of the empty strip below the rectangle
+    public boolean reject(ADT_Area area, ADT_Rectangle last) {
+        this.area = (ADT_AreaExtended) area;
+        emptySpaceHeight = calculateEmptySpaceHeight((ADT_AreaExtended) area, last); // height of the empty strip on the left of the rectangle
+        emptySpaceWidth = calculateEmptySpaceWidth((ADT_AreaExtended) area, last); // width of the empty strip below the rectangle
         RectangleType[] rectanglesToBePlaced = this.area.getRectangleTypesToBePlaced(); // rectangles to be placed
         boolean rotations = area.canFlip(); // rotations are allowed
 
         if (!rotations) {
             ArrayList<RectangleType> rectangleFitsHeight = new ArrayList<>(); // array list which stores the rectangles with height <= height empty strip
 
-            for (RectangleType type : rectanglesToBePlaced) { // add all rectangles <= height of the empty strip
-                if (type.getHeight() <= emptySpaceHeight) {
-                    rectangleFitsHeight.add(type);
+            for (int i = 0; i < rectanglesToBePlaced.length; ++i) { // add all rectangles <= height of the empty strip
+                if (rectanglesToBePlaced[i].getHeight() <= emptySpaceHeight) {
+                    rectangleFitsHeight.add(rectanglesToBePlaced[i]);
                 }
             }
             ArrayList<RectangleType> rectangleFitsWidth = new ArrayList<>(); // array list which stores the rectangles with width <= width empty strip
 
-            for (RectangleType type : rectanglesToBePlaced) { // adds all rectangles <= width to the empty strip
-                if (type.getWidth() <= emptySpaceWidth) {
-                    rectangleFitsWidth.add(type);
+            for (int i = 0; i < rectanglesToBePlaced.length; i++) { // adds all rectangles <= width to the empty strip
+                if (rectanglesToBePlaced[i].getWidth() <= emptySpaceWidth) {
+                    rectangleFitsWidth.add(rectanglesToBePlaced[i]);
                 }
             }
         } else {
             ArrayList<RectangleType> rectangleFitsBottom = new ArrayList<>(); // stores rectangles that fit into the bottom empty strip
             ArrayList<RectangleType> rectangleFitsLeft = new ArrayList<>(); // stores rectangles that fit into the left empty strip
 
-            for (RectangleType type : rectanglesToBePlaced) {
-                if (type.getHeight() <= emptySpaceHeight || type.getWidth() <= emptySpaceHeight) {
-                    rectangleFitsBottom.add(type); // width or height <= width means a rectangle fits
+            for (int i = 0; i < rectanglesToBePlaced.length; i++) {
+                if (rectanglesToBePlaced[i].getHeight() <= emptySpaceHeight || rectanglesToBePlaced[i].getWidth() <= emptySpaceHeight) {
+                    rectangleFitsBottom.add(rectanglesToBePlaced[i]); // width or height <= width means a rectangle fits
                 }
-                if (type.getWidth() <= emptySpaceWidth || type.getHeight() <= emptySpaceWidth) {
-                    rectangleFitsLeft.add(type);
+                if (rectanglesToBePlaced[i].getWidth() <= emptySpaceWidth || rectanglesToBePlaced[i].getHeight() <= emptySpaceWidth) {
+                    rectangleFitsLeft.add(rectanglesToBePlaced[i]);
                 }
             }
         }
         // if all future placements fit below or to the left of the candidate rectangle
         // then reject (return true)
-        return true; // TODO: call the algorithm to perform rectangle packing with array list of rectangles and empty strip dimensions as input.
-                    // Return true there is at least one solution.
+        return false;
+        // Return true there is at least one solution.
     }
 
     /**
@@ -61,36 +65,35 @@ public class Strat_BT_Pruner_NarrowEmptyStrips implements Strat_BT_PrunerInterfa
     private int calculateEmptySpaceHeight(ADT_AreaExtended area, ADT_Rectangle last){
         int lowestPoint = Integer.MAX_VALUE; // lowest point of possible empty strip
         int beginY = last.getY();
-        int endY = 0; // the empty strip may
+        int endY = 0; // the empty strip may end at the bottom
 
         // Scan vertically to find the bottom of the empty strip
-        int x = last.getX();
-        if(x == 0) return 0; // There is no dominance below possible
+        if(beginY == 0) return 0; // There is no dominance below possible
 
         while(lowestPoint == Integer.MAX_VALUE){ // We haven't reached the bottom of the empty strip
-            x--;
+            beginY--;
 
-            for (int y = beginY; y > endY; --y) {
+            for (int y = beginY; y => endY; --y) {
                 if (!area.isEmptyAt(x, y) && y == beginY) {
                     // Possibly the beginning of a row of borders
-                    lowestPoint = x;
+                    lowestPoint = y;
                 } else if(!area.isEmptyAt(x, y) && lowestPoint == Integer.MAX_VALUE) {
                     /*
                     We encountered a border halfway on the line, but this line hasn't
                     seen a border yet. This means the rectangle is not perfect.
                     */
-                    return x;
+                    return last.getY() - y;
                 }
             }
             // The left side of the bounding rectangle is a good leftmost point
-            if(x-1 == 0) lowestPoint = x;
+            if(y - 1 == 0) lowestPoint = 0;
         }
         // return the height of the empty strip
-        return x;
+        return last.getY() - lowestPoint;
     }
 
     /**
-     * Calculates the width of the empty strip
+     * Calculates the width of the empty strip.
      *
      * @param last rectangle to look left for empty strip of rectangle
      * @return width of the empty strip
@@ -100,36 +103,72 @@ public class Strat_BT_Pruner_NarrowEmptyStrips implements Strat_BT_PrunerInterfa
         int beginX = last.getX();
         int endX = 0;
 
-        // Scan horizontally to find bottom of a possibly perfect rectangle
-        int y = last.getY();
+        // Scan horizontally to find the leftmost point of the strip
+        if(beginx == 0) return 0; // There is no dominance below possible
 
-        if(y == 0) return 0; // There is no dominance below possible
+        while (leftMostPoint == Integer.MAX_VALUE) { // We haven't reached the left of the rectangle
+            beginX--;
 
-        while (leftMostPoint == Integer.MAX_VALUE) { // We haven't reached the bottom of the rectangle
-            y--;
-
-            // First check right and left border
-            if(!(area.isEmptyAt(beginX-1, y) && area.isEmptyAt(beginX+endX, y))) {
-                // This must be the lowest level, otherwise it is not valid  (missing side border)
-                leftMostPoint = y;
-            }
-
-            for(int x = beginX; x < endX; ++x) {
+            for(int x = beginX; x => endX; --x) {
                 if(!area.isEmptyAt(x, y) && x == beginX) {
                     // This possibly the beginning of a line of borders
-                    leftMostPoint = y;
+                    leftMostPoint = x;
                 } else if(!area.isEmptyAt(x, y) && leftMostPoint == Integer.MAX_VALUE) {
                     /*
                     We encountered a border halfway on the line, but this line hasn't
                     seen a border yet. This means the rectangle is not perfect.
                     */
-                    return y;
+                    return last.getX() -  x;
                 }
             }
             // The bottom of the bounding rectangle is a good bottom
-            if (y - 1 == 0) leftMostPoint = y;
+            if (x - 1 == 0) leftMostPoint = 0;
         }
         // return the width of the empty strip
-        return y;
+        return last.getX() - leftMostPoint;
+    }
+
+    /**
+     * Creates a separate area with the dimensions of the empty strip.
+     *
+     * @param area, last, emptySpaceHeight
+     * @return area with width of the last placed rectangle, calculated height of the empty space
+     */
+    public SetStripAreaBottom(ADT_AreaExtended area, ADT_Rectangle last, int emptySpaceHeight) {
+        this.area = area; // SET ARRAY LIST OF RECTANGLES TO ARRAY OF AREA
+        this.last.getX() = last.getX();
+        this.emptySpaceHeight = calculateEmptySpaceHeight(ADT_AreaExtended area, ADT_Rectangle last);
+        // if (!rotations) [] rectangleFitsHeight = areaSpaceBelow;  rectangleFitsWidth = areaSpaceLeft
+        // else [] rectangleFitsBottom = areaSpaceBelow; rectangleFitsLeft = areaSpaceLeft;
+    }
+
+    public void getStripAreaBottom() {
+        return false;
+    }
+
+    /**
+     * Creates a separate area with the dimensions of the empty strip.
+     *
+     * @param area, last, emptySpaceWidth
+     * @return area with width of the last placed rectangle, calculated height of the empty space
+     */
+    public SetStripAreaLeft(ADT_AreaExtended area, ADT_Rectangle last, int emptySpaceWidth) {
+        this.area = area; // SET ARRAY LIST OF RECTANGLES TO ARRAY OF AREA
+        this.last.getY() = last.getY();
+        this.emptySpaceWidth = calculateEmptySpaceHeight(ADT_AreaExtended area, ADT_Rectangle last);
+    }
+
+    public void getStripAreaBottom() {
+        return false;
+    }
+
+    /**
+     * Calls the algorithm on both setStripAreaBottom and setStripAreaLeft to perform rectangle packing with array list of rectangles as input.
+     * Returns whether a solution can be found and stops.
+     *
+     * @return
+     */
+    public boolean callAlgorithm() {
+        return false;
     }
 }
